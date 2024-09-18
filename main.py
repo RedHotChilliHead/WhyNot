df5159d113f2a612672adaeed359205a0b7ec8d5
Red <irina.purgina@inbox.ru>
2024-09-18 14:17:27 +0300

    edit README

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# from fastapi import FastAPI
-#
-# app = FastAPI()
-#
-#
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-#
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
-
 from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 from time import monotonic
@@ -36,7 +22,7 @@ async def work() -> None:
 @router.get("/test", response_model=TestResponse)  # предоставление метода GET /test
 async def handler() -> TestResponse:
     ts1 = monotonic()
-    async with lock:  # Не допускаем одновременной работы нескольких функций
+    async with lock:  # не допускаем одновременной работы нескольких функций
         await work()
     ts2 = monotonic()
     return TestResponse(elapsed=ts2 - ts1)
@@ -45,4 +31,3 @@ app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-    # для запуска приложения uvicorn main:app --reload
